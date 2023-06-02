276a43ff1253c2767447e66ea7608fbdce2f5c36
H4N9U1 <h4n9u1@H4N9U1s-MacBook-Pro.local>
2023-06-02 13:30:43 +0900

    VirsuTotal 일별 제한 대응 코드 적용

--- classifier/classifier/__main__.py
+++ classifier/classifier/__main__.py
@@ -19,27 +19,29 @@ collection = db['malware']
 initial_files = os.listdir(file_dir)
 
 while True:
-    current_files = os.listdir(file_dir)
+    for _ in range(500): # Virustotal Daliy Limits
+        current_files = os.listdir(file_dir)
 
-    new_files = set(current_files) - set(initial_files)
+        new_files = set(current_files) - set(initial_files)
 
-    if new_files:
-        for file in new_files:
+        if new_files:
+            for file in new_files:
 
-            uploaded_path = requests.post(f'{api_url}/scan/upload', files={'file': open(os.path.join(file_dir, f'{file}'), 'rb')}).json()['path']
+                uploaded_path = requests.post(f'{api_url}/scan/upload', files={'file': open(os.path.join(file_dir, f'{file}'), 'rb')}).json()['path']
 
-            result = {}
+                result = {}
 
-            result['timestamp'] = int(time.time())
-            result['ip'] = requests.get(f'{api_url}/platform/ip').json()
-            result['fileinfo'] = requests.get(f'{api_url}/scan/info', params={'path': uploaded_path}).json()
-            result['virustotal'] = requests.get(f'{api_url}/scan/vt', params={'path': uploaded_path}).json()
-            result['malwarebazaar'] = requests.get(f'{api_url}/scan/mb', params={'path': uploaded_path}).json()
-            result['prediction'] = requests.get(f'{api_url}/scan/prediction', params={'path': uploaded_path}).json()
+                result['timestamp'] = int(time.time())
+                result['ip'] = requests.get(f'{api_url}/platform/ip').json()
+                result['fileinfo'] = requests.get(f'{api_url}/scan/info', params={'path': uploaded_path}).json()
+                result['virustotal'] = requests.get(f'{api_url}/scan/vt', params={'path': uploaded_path}).json()
+                result['malwarebazaar'] = requests.get(f'{api_url}/scan/mb', params={'path': uploaded_path}).json()
+                result['prediction'] = requests.get(f'{api_url}/scan/prediction', params={'path': uploaded_path}).json()
 
-            # upload file to API
-            document = collection.insert_one(result)
+                # upload file to API
+                document = collection.insert_one(result)
 
-        initial_files = current_files
+            initial_files = current_files
 
-    time.sleep(1)
+        time.sleep(15) # Wait for 15sec (4 samples / min)
+    time.sleep(86400) # Sleep 24 hours
