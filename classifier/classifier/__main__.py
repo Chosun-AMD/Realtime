52514b1b699aeffb2096997c85238ca9aedd8ecd
H4N9U1 <h4n9u1@H4N9U1s-MacBook-Pro.local>
2023-06-10 21:04:09 +0900

    라이선스 추가 (AGPL)

--- classifier/classifier/__main__.py
+++ classifier/classifier/__main__.py
@@ -2,6 +2,11 @@ import os
 import requests
 import time
 from pymongo import MongoClient
+import mimetypes
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+from tqdm.auto import tqdm
+import glob
+import hashlib
 
 api_url = os.environ.get('API_URL', 'http://localhost:8000')
 file_dir = os.environ.get('DATA_DIR', '/data')
@@ -16,35 +21,43 @@ client = MongoClient(host=host, port=port, username=username, password=password)
 db = client[db_name]
 collection = db['malware']
 
-initial_files = os.listdir(file_dir)
+initial_files = []
 
 count = 0
 while True:
-    current_files = os.listdir(file_dir)
+    try:
+        print(file_dir)
+        current_files = glob.glob(file_dir + '/*')
 
-    new_files = set(current_files) - set(initial_files)
+        new_files = set(current_files) - set(initial_files)
 
-    if new_files:
-        for file in new_files:
+        if new_files:
+            print(len(current_files))
+            for file in tqdm(new_files):
+                print(file)
+                files = {'file': (file, open(os.path.join(file_dir, file), 'rb'))}
 
-            uploaded_path = requests.post(f'{api_url}/scan/upload', files={'file': open(os.path.join(file_dir, f'{file}'), 'rb')}).json()['path']
+                uploaded_path = requests.post(f'{api_url}/scan/upload', files=files).json()['path']
 
-            result = {}
+                result = {}
 
-            result['timestamp'] = int(time.time())
-            result['ip'] = requests.get(f'{api_url}/platform/ip').json()
-            result['fileinfo'] = requests.get(f'{api_url}/scan/info', params={'path': uploaded_path}).json()
-            result['virustotal'] = requests.get(f'{api_url}/scan/vt', params={'path': uploaded_path}).json()
-            result['malwarebazaar'] = requests.get(f'{api_url}/scan/mb', params={'path': uploaded_path}).json()
-            result['prediction'] = requests.get(f'{api_url}/scan/prediction', params={'path': uploaded_path}).json()
+                result['timestamp'] = int(time.time())
+                result['ip'] = requests.get(f'{api_url}/platform/ip').json()
+                # result['id'] = hashlib.sha256(open(os.path.join(file_dir, file), 'rb').read()).hexdigest()
+                result['fileinfo'] = requests.get(f'{api_url}/scan/info', params={'path': uploaded_path}).json()
+                # result['virustotal'] = requests.get(f'{api_url}/scan/vt', params={'path': uploaded_path}).json()
+                # result['malwarebazaar'] = requests.get(f'{api_url}/scan/mb', params={'path': uploaded_path}).json()
+                result['prediction'] = requests.get(f'{api_url}/scan/prediction', params={'path': uploaded_path}).json()
 
-            # upload file to API
-            document = collection.insert_one(result)
+                # upload file to API
+                document = collection.insert_one(result)
 
-        initial_files = current_files
+            initial_files = current_files
+    except Exception as e:
+        pass
 
-        time.sleep(15) # Wait for 15sec (4 samples / min)
-        count += 1
-    if count == 500:
-        time.sleep(86400) # Sleep 24 hours
-        count = 0
+    #     time.sleep(15) # Wait for 15sec (4 samples / min)
+    #     count += 1
+    # if count == 500:
+    #     time.sleep(86400) # Sleep 24 hours
+    #     count = 0
